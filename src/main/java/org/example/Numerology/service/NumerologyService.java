ffd090a1a9711401622917a1ad2f6bd662cc23f5
Vinayak Kedia <vinayak@Vinayaks-MacBook-Air.local>
2026-09-24 13:50:10 +0530

    Number logic addition

--- src/main/java/org/example/Numerology/service/NumerologyService.java
+++ src/main/java/org/example/Numerology/service/NumerologyService.java
@@ -130,6 +130,15 @@ public class NumerologyService {
             case 'O', 'Z' -> 7;
 
             case 'F', 'P' -> 8;
+            case '1' -> 1;
+            case '2' -> 2;
+            case '3' -> 3;
+            case '4' -> 4;
+            case '5' -> 5;
+            case '6' -> 6;
+            case '7' -> 7;
+            case '8' -> 8;
+            case '9' -> 9;
 
             default -> 0;
         };
